7a26a280e68c3869f068a6da021d9d12be4e200a
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-06-06 17:48:30 +0330

    update[.eslintrc.cjs]-> Update eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,39 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+  "root": true,
+  "env": { browser: true, es2020: true },
+  "extends": [
+       "eslint:recommended",
+       "plugin:import/errors",
+       "plugin:react-recommended",
+       "plugin:jsx-a11y/recommended",
+       "prettier"
+   ],
+   "rules": {
+       "react/prop-types": 0,
+       "react/react-in-jsx-scope": 0
+   },
+   "plugins": ["react","import","jsx-a11y"],
+   "parserOptions": {
+     "ecmaVersion": 2022,
+     "sourceType": "module",
+     "ecmaFeatures": {
+       "jsx": true
+     }
+   },
+   "env": {
+     "es6": true,
+     "browser": true,
+     "node": true
+   },
+   "settings": {
+       "react": {
+           "version": "detect"
+       },
+       "import/resolver": {
+           "node": {
+               "extensions": [".js", ".jsx", ".ts", ".tsx"]
+           }
+       }
+   }
+
 }
