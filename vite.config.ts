a3d928c49fd484e66bda6264f48f37a8a32dd05e
Mobin Karimi <mobinkaarimi@gmail.com>
2024-08-27 23:44:10 +0330

    update[package.json,vite.config.ts]-> try to fix deploy bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/mobinkaarimi/react-checkboxify/tree/examples",
   plugins: [react()],
+  base: "https://github.com/mobinkaarimi/react-checkboxify/tree/examples",
+
 });
