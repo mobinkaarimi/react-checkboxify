5496c50642aa611525ea2ed29801c020589767c5
Mobin Karimi <mobinkaarimi@gmail.com>
2024-08-28 02:25:46 +0330

    update[vite.config.ts]-> revert base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mobinkaarimi.github.io/react-checkboxify/",
+  base: "/react-checkboxify/",
 });
