e91380f906f337296942115582e8dd71d454c625
Mobin Karimi <mobinkaarimi@gmail.com>
2024-08-27 22:38:46 +0330

    update[tailwind.config.js]-> add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-orange": "#F4A012",
+      },
+    },
   },
   plugins: [],
 };
